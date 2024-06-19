2f10ecdcca098cbb661c2ffa814628e5b5c700a9
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2024-06-19 16:03:00 +0530

    Update DocumentReader.py

--- DocumentsReader/DocumentReader.py
+++ DocumentsReader/DocumentReader.py
@@ -1,8 +1,9 @@
 import os
+import re
+import json
 import torch
 from PIL import Image
 from transformers import AutoModel, AutoTokenizer
-from helper import convert_file_to_images
 
 def Load():
   # Initialize the model and tokenizer
@@ -12,30 +13,33 @@ def Load():
   return model, tokenizer
 
 def DocumentReader(FileName):
+    convert_file_to_images(FileName)
+    # Initialize the model and tokenizer
+    model, tokenizer = Load()
+    # Question to ask for each image
+    question = 'Give data in json?'
+    msgs = [{'role': 'user', 'content': question}]
 
-  convert_file_to_images(FileName)
-  # Initialize the model and tokenizer
-  model, tokenizer = Load()
+    output_folder = 'output_folder'
+    final_json = {}
 
-  # Question to ask for each image
-  question = 'Give data in json?'
-  msgs = [{'role': 'user', 'content': question}]
-  
-  output_folder = 'output_folder'
-  # Process each image in the folder
-  for filename in os.listdir(output_folder):
-      if filename.endswith('.png'):
-          image_path = os.path.join(output_folder, filename)
-          image = Image.open(image_path).convert('RGB')
-          
-          response = model.chat(
-              image=image,
-              msgs=msgs,
-              tokenizer=tokenizer,
-              sampling=True,
-              temperature=0.7
-          )
-          
-          return f"Response for {filename}:" , response
-          
-          
+    # Process each image in the folder
+    for filename in os.listdir(output_folder):
+        if filename.endswith('.png'):
+            image_path = os.path.join(output_folder, filename)
+            image = Image.open(image_path).convert('RGB')
+
+            response = model.chat(
+                image=image,
+                msgs=msgs,
+                tokenizer=tokenizer,
+                sampling=True,
+                temperature=0.7
+            )
+            json_match = re.search(r'\{[\s\S]*\}', response)
+            image_json = json_match.group(0) if json_match else ""
+            # Assuming the response is a valid JSON string
+            image_json = json.loads(image_json)
+            final_json[filename] = image_json
+
+    return final_json
